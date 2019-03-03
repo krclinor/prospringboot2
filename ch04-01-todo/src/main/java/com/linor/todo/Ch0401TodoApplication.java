bf8b56a466743380cc328a8de1c2c97a457c6076
krclinor <krclinor@gmail.com>
2019-03-03 18:35:02 +0900

    todo 어플리케이션 생성

---
+++ ch04-01-todo/src/main/java/com/linor/todo/Ch0401TodoApplication.java
@@ -0,0 +1,13 @@
+package com.linor.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch0401TodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch0401TodoApplication.class, args);
+	}
+
+}

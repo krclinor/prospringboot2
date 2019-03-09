c2156dca06bee5728c3c31059580dca7c13c06d6
krclinor <krclinor@gmail.com>
2019-03-09 16:23:29 +0900

    github의 Oauth2인증처리

---
+++ ch08-06-oauth2/src/main/java/com/linor/todo/Ch0806Oauth2Application.java
@@ -0,0 +1,13 @@
+package com.linor.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch0806Oauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch0806Oauth2Application.class, args);
+	}
+
+}

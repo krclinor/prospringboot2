15f12ee6439b791718a147bebe165485f470c75a
krclinor <krclinor@gmail.com>
2019-03-09 16:21:58 +0900

    사용자정의 로그인페이지

---
+++ ch08-03-custom-login/src/main/java/com/linor/todo/Ch0801SecApplication.java
@@ -0,0 +1,13 @@
+package com.linor.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch0801SecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch0801SecApplication.class, args);
+	}
+
+}
---
+++ ch08-03-custom-login/src/main/java/com/linor/todo/config/ToDoSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.linor.todo.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(passwordEncoder())
+			.withUser("linor")
+			.password(passwordEncoder().encode("init11"))
+			.roles("ADMIN","USER");
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.authorizeRequests()
+			.requestMatchers(
+					PathRequest
+						.toStaticResources()
+						.atCommonLocations()).permitAll()
+			.anyRequest().fullyAuthenticated()
+			.and()
+			.formLogin().loginPage("/login").permitAll()
+			.and()
+			.logout()
+				.logoutSuccessUrl("/login")
+			.and()
+				.httpBasic();
+	}
+	
+	
+}
